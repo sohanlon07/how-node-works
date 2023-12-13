5ddfd3858defbe06f1c3aa2aefd08453135e1fe2
shaneohanlon-77276 <shane.ohanlon@synechron.com>
2023-12-13 17:34:50 +0000

    remove Robby

--- events.js
+++ events.js
@@ -1,13 +1,40 @@
 const EventEmitter = require('events');
+const http = require('http');
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
 
 myEmitter.on('newSale', () => {
-    console.log('There was a new sale!');
+  console.log('There was a new sale!');
 });
 
 myEmitter.on('newSale', () => {
-    console.log('Customer name: Jonas');
+  console.log('Customer name: Jonas');
 });
 
-myEmitter.emit('newSale');
+myEmitter.on('newSale', (stock) => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
+
+myEmitter.emit('newSale', 9);
+
+/////////////////////////////
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log('Request received');
+  res.end('Request received');
+});
+
+server.on('request', (req, res) => {
+  res.end('Another Request');
+});
+
+server.on('close', () => {
+  console.log('Server Closed');
+});
